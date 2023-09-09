f434ff89a79e6fa0481790b539781911e67a026c
farildev <memmedovfaril@gmail.com>
2023-09-10 00:06:33 +0400

    Some bugs fixed

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,43 +1,43 @@
 export default [
 	{
 		id: 1,
-		title: 'Intouchables - Soundtracks OST',
+		title: "Car's Outside",
 		description: 'Original Soundtrack',
 		artist: 'James Arthur',
-		image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+		image: 'https://i.scdn.co/image/ab676161000051744a88c3a1fc06ab0be1307e19',
 		type: 'album',
 		src: 'https://freesound.org/data/previews/612/612095_5674468-lq.mp3'
 	},
 	{
 		id: 2,
-		title: 'Peaceful Piano',
+		title: 'Daylight',
 		artist: 'David Kushner',
 		description: 'Relax and indulge with beautiful piano pieces',
-		image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
+		image: 'https://i.scdn.co/image/ab67616100005174f6a68d90ef0bb84219e3499a',
 		type: 'album',
 		src: 'https://freesound.org/data/previews/612/612092_7037-lq.mp3'
 	},
 	{
 		id: 3,
-		title: 'Hang Massive',
+		title: 'Way Down We Go',
 		artist: 'KALEO',
 		description: 'Artist',
-		image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
+		image: 'https://i.scdn.co/image/ab67616100005174c2bc8368a241196dd45d6f62',
 		type: 'artist',
 		src: 'https://freesound.org/data/previews/612/612087_1648170-lq.mp3'
 	},
 	{
 		id: 4,
-		title: 'Intouchables - Soundtracks OST',
+		title: 'The Weekend - Call Out My Name',
 		artist: 'The Weeknd',
 		description: 'Original Soundtrack',
-		image: 'https://i.scdn.co/image/d39397d2f6a1525b3fe90369c89ea2d94aac5714',
+		image: 'https://i.scdn.co/image/ab67616100005174214f3cf1cbe7139c1e26ffbb',
 		type: 'podcast',
 		src: 'https://freesound.org/data/previews/612/612085_28867-lq.mp3'
 	},
 	{
 		id: 5,
-		title: 'Intouchables - Soundtracks OST',
+		title: 'Under The Influence',
 		artist: 'Chris Brown',
 		description: 'Original Soundtrack',
 		image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
